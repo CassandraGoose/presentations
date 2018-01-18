98d49b2b36a4631d8957858e4ce4d0350c5a892b
Cassandra Torske <cassandra.torske@gmail.com>
2018-01-17 21:55:30 -0700

    add videos; adjust component for videos; prep for more styling

--- src/components/LeftMenu.js
+++ src/components/LeftMenu.js
@@ -17,22 +17,40 @@ class LeftMenu extends Component {
         <Sidebar.Pushable>
           <Sidebar as={Menu} animation='slide along' width='thin' visible={visible} icon='labeled' vertical inverted>
             <Menu.Item name='home'>
-              <Icon name='home' />
               <Link to="/landing">Home</Link>
             </Menu.Item>
-            <Menu.Item name='about'>
-              <Icon name='about' />
-              <Link to="/video1">Video "1"</Link>
+            <Menu.Item name='video1'>
+              <Link to="/video/yNiO2jivXsU">Intro to Sorting</Link>
             </Menu.Item>
-            <Menu.Item name='contact'>
-              <Icon name='contact' />
-              Video "2"
+            <Menu.Item name='video2'>
+              <Link to="/video/TAaadUhcnOg">Intro to Express</Link>
+            </Menu.Item>
+            <Menu.Item name='video3'>
+              <Link to="/video/jYDldOM4lhM">The Accumulator Pattern Walkthrough</Link>
+            </Menu.Item>
+            <Menu.Item name='video4'>
+              <Link to="/video/xaOfeVVzPYY">Intro to AJAX Walkthrough</Link>
+            </Menu.Item>
+            <Menu.Item name='video5'>
+              <Link to="/video/GDg6alKEnw8">Intro to SQL</Link>
+            </Menu.Item>
+            <Menu.Item name='video6'>
+              <Link to="/video/M5Np7xguHz4">Validations Overview</Link>
+            </Menu.Item>
+            <Menu.Item name='video7'>
+              <Link to="/video/wQsYZyzkpLM">Intro to HTML Forms</Link>
+            </Menu.Item>
+            <Menu.Item name='video8'>
+              <Link to="/video/ATXiCaa0R-c">DOM & jQuery Breakout</Link>
+            </Menu.Item>
+            <Menu.Item name='video9'>
+              <Link to="/video/-qZumxVyKlU">Overview of es2015 Breakout</Link>
             </Menu.Item>
           </Sidebar>
           <Sidebar.Pusher>
             <Segment basic>
             <Route path="/landing" component={Splash}/>
-            <Route path="/video1" component={VideoList}/>
+            <Route path="/video/:id" component={VideoList}/>
             </Segment>
           </Sidebar.Pusher>
         </Sidebar.Pushable>
@@ -40,5 +58,8 @@ class LeftMenu extends Component {
     )
   }
 }
+
+
+
 
 export default LeftMenu
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Embed, Container } from 'semantic-ui-react'
 
-const VideoList = () => (
-  <Embed
-    id='M5Np7xguHz4'
+const VideoList = (props) => {
+  return (
+    <Embed
+    id={props.match.params.id}
     source='youtube'
+    icon='rocket'
   />
-)
+  )
+}
 
 export default VideoList
